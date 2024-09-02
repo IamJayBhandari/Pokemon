d0c678c2446deb3266d66466453178c20dc264ce
IamJayBhandari <111848597+IamJayBhandari@users.noreply.github.com>
2024-09-02 14:07:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'src', // Set root to 'src' if index.html is in 'src'
   base: '/pokemon/', // Adjust if necessary
 })
