37a3c40ac6d7a4388ac1ba1bd094a80c63d0195a
IamJayBhandari <111848597+IamJayBhandari@users.noreply.github.com>
2024-09-02 13:48:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pokemon/', // Replace 'pokemon' with your GitHub repository name
+  base: '/pokemon/', // Adjust if necessary
 })
