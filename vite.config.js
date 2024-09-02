20681e3214897b642b019c5fbb68a70e93c10186
IamJayBhandari <111848597+IamJayBhandari@users.noreply.github.com>
2024-09-02 14:13:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pokemon/', // Adjust this if necessary
+  root: '.', // Ensure the root is correctly set
+  build: {
+    outDir: 'dist', // Output directory for build
+  },
+  base: '/', // Adjust if needed based on GitHub Pages setup
 })
