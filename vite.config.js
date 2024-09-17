204994d440792e7fee0fbfedf5d947b0798d6c05
IamJayBhandari <111848597+IamJayBhandari@users.noreply.github.com>
2024-09-17 18:23:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,14 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   root: '.', // Ensure the root is correctly set
-//   build: {
-//     outDir: 'dist', // Output directory for build
-//   },
-//   base: '/',
-// })
-// vite.config.js
-export default {
-  base: './', 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: '.', // Ensure the root is correctly set
   build: {
-    rollupOptions: {
-      external: ['/src/main.<hash>.js'],  // Replace <hash> with the actual hash if it's static
-    }
-  }
-};
+    outDir: 'dist', // Output directory for build
+  },
+  base: '/',
+})
+vite.config.js
+
