30e02a26403cf2fee394863871083a3bd1021f63
IamJayBhandari <111848597+IamJayBhandari@users.noreply.github.com>
2025-01-31 12:49:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,6 @@
 // });
 
 
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -23,10 +22,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure the build output directory is `dist`
+    outDir: 'dist', // Output directory
     rollupOptions: {
       input: '/index.html', // Explicitly specify index.html for entry point
     },
   },
-  base: './', // Use relative paths for assets
+  base: '/',  // This ensures assets are loaded from the root
 });
