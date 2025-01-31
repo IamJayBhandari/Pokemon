c91049c4c93848f3c66bd140a32504388cee7a08
IamJayBhandari <jaybhandari2404@gmail.com>
2025-01-31 12:21:42 +0530

    v2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',  // Ensure the build output directory is `dist`
+    rollupOptions: {
+      input: '/index.html',  // Explicitly specify index.html for entry point
+    },
+  },
+  base: './',  // Use relative paths for assets
+});
